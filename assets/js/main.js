08d14775b6a82c48b024361c443172df4ef98be9
MiticoPlayer <aperper.4@gmail.com>
2022-08-11 11:25:59 +0200

    fix: Some front fails

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,23 @@ function getScreenSize() {
   $("#div3").height(height * 2);
 }
 
+function getBreeds(name) {
+  const url = `https://api.thecatapi.com/v1/images?/search?breed_id=${name}`;
+  fetch(url);
+  headers["x-api-key"] = "fd0dd5ce-a4a6-4fce-8682-81ae5799bea3"
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
 window.addEventListener("load", () => {
   getScreenSize();
+  raza = "beng";
+  getBreeds(raza);
 });
